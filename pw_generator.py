6f3ea62a212cb550b25f73a9cf9bd60c2d5a36fe
Alexander Jacks <alexanderthejacks@gmail.com>
2019-10-17 00:13:06 -0700

    :wrench: better MVP, streamlined app

--- pw_generator.py
+++ pw_generator.py
@@ -7,8 +7,12 @@ Allow the user to choose how many characters the password will be.
 Advanced Version 2
 Allow the user to choose how many letters, numbers, and punctuation characters they want in their password. Mix everything up using list(), random.shuffle(), and ''.join().
 '''
+# To use the nifty copy/paste feature, please first run:
+# $ sudo pip3 install pyperclip
+# in your Terminal
 import random
 import string
+# import pyperclip
 lowers = string.ascii_lowercase
 uppers = string.ascii_uppercase
 punctuation = ["!","@","#","$","%","^","&","*","(",")","~","+","{","}","[","]","-","|","`","="]
@@ -22,6 +26,11 @@ if 10 <= counter <= 256:
         inprogress_password.append(random.choice(possible_characters))
         counter -= 1
     finalized_password = ''.join(inprogress_password)
+# plz uncomment for nifty feature where it copies pw to your clipboard automatically. Plz install pyperclip 1st!
+    # pyperclip.copy(finalized_password)
+
     print(f"Here's a randomized, {remember_their_choice}-character password: {finalized_password}")
+    # ditto on uncomment below for autocopy
+    # print("\nWe've copied it (ya know, Ctrl+C'd it) to your clipboard, so Paste it somewhere safe!\nOr go ahead and drag over the password and copy/paste it yourself, we know it's tough to trust open source software. 🤖💦")
 else:
     print("Please try this script again, and enter a number bigger than 10 and smaller than 2^8")
